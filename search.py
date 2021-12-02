afcc98aab5747688e7a48bc0a577eac54133b313
puloma-k <puloma@pulomakatiyar.com>
2021-12-01 23:11:15 -0800

    Added cosine similarity ranking

--- search.py
+++ search.py
@@ -1,29 +1,52 @@
 import pickle
 import os
+import math
 from pprint import pprint
+from scipy import spatial
 from nltk.tokenize import word_tokenize
 from indexer import indexer, getTokenPath
+from collections import defaultdict
 from nltk.stem.porter import *
 import traceback
 
 INDEX_ROOT_PATH = "/Users/puloma/Code/CS121/Assignment #3/index/part0"
+SECTION_REGULAR_WEIGHT = 1
+SECTION_BOLD_WEIGHT = 1.25
+SECTION_TITLE_WEIGHT = 1.5
+SECTION_HEADING_WEIGHT = 1.75
+NUM_TOTAL_DOCS = 5
+
+def cosineSimilarity(query_tfidf, all_tfidf_in_doc):
+    # print("query tfidf")
+    # print(query_tfidf)
+    # print("all tfidf")
+    # print(all_tfidf_in_doc)
+    return (1 - spatial.distance.cosine(query_tfidf, all_tfidf_in_doc))
 
 def retriever(tokens_list):
     # for each token in list, get postings list
     all_tokens_postings = []
+    query_tokens_freq = defaultdict(int)
+    for token in tokens_list:
+        query_tokens_freq[token] += 1
+
+    # calculate query tfidf
+    query_tfidf = []
     for token in tokens_list:
         token_file_path = INDEX_ROOT_PATH + "/" + getTokenPath(token)
         if not os.path.exists(token_file_path):
             return []
         with open(token_file_path, 'rb') as path:
-            all_tokens_postings.append(pickle.load(path))
+            token_posting = pickle.load(path)
+            all_tokens_postings.append(token_posting)
+            query_tfidf.append((1 + math.log10(query_tokens_freq[token])) * math.log10(NUM_TOTAL_DOCS / len(token_posting)))
     
     # take all curent postings and put in list
     # check if docs of all curr postings are the same
     # if so: add doc id in results list, and increment all indexes in index list
     # else: find first min doc id and increment corresponding index in index list
     # clear curr postings   
-    results_list = set()
+    results_list = {}
     curr_postings = []
     index_list = [0 for token in tokens_list]
     end_not_reached = True
@@ -42,7 +65,12 @@ def retriever(tokens_list):
                         end_not_reached = False
                         break
             # add doc ID to results list
-            results_list.add(curr_postings[0][0])
+            posting_for_doc = curr_postings[0]
+            all_tfidf_in_doc = []
+            for posting in curr_postings:
+                all_tfidf_in_doc.append((posting[1] * SECTION_REGULAR_WEIGHT) + (posting[2] * SECTION_TITLE_WEIGHT) + (posting[3] * SECTION_HEADING_WEIGHT) + (posting[4] * SECTION_BOLD_WEIGHT))
+            cos_sim_val = cosineSimilarity(query_tfidf, all_tfidf_in_doc)
+            results_list[posting_for_doc[0]] = cos_sim_val
         # if doc IDs don't match, then increment index pointer of all postings lists that
         # currently point to a posting with min doc ID
         else:
@@ -77,8 +105,9 @@ def main():
         result_docs = retriever(tokens_list)
         if not result_docs:
             print("No results found")
-        for doc_id in result_docs:
-            print(url_dict[doc_id] + "\n")
+        else:
+            for doc_id, cos_sim_val in sorted(result_docs.items(), key=lambda x:x[1], reverse=True):
+                print(url_dict[doc_id] + " " + str(cos_sim_val) + "\n")
 
 if __name__ == "__main__":
     main()
